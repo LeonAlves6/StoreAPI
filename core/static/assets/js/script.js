de0539186704603961641caad4ac037f22e7a602
LeonardoMartell <leonardoalvesaraujo@hotmail.com>
2026-06-22 21:00:35 -0300

    teste4

---
+++ core/static/assets/js/script.js
@@ -0,0 +1,19 @@
+/*let email = 'leonardoalvesaraujo@hotmail.com'
+let menu_button = document.querySelector('#menu-button')
+let email_button = document.querySelector('#botao-email')
+
+async function copiarTexto(){
+    try{
+        await navigator.clipboard.writeText(email)
+    } catch(err){
+        console.error("Falha ao copiar texto: ", err)
+    }
+}
+
+menu_button.addEventListener('click', ()=>{
+    alert('Botão funcionando')
+})
+
+email_button.addEventListener('click', ()=>{
+    alert('Email copiado')
+})*/
--- teste/urls.py
+++ teste/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, Portfolio
 
 urlpatterns = [
     path('home', index, name="home"),
+    path('portfolio', Portfolio, name="portfolio"),
 ]
